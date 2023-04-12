ca513d1864608d3b50177e690436b29a5df863a9
Matthew Lee <mlee2197@gmail.com>
2023-04-11 22:27:50 -0400

    complete challenge 1

--- myApp.js
+++ myApp.js
@@ -1,38 +1,8 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+app.get("/", function (req, res) {
+  res.send("Hello Express");
+});
 
-
-
-
-
-
-console.log("Hello World");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
